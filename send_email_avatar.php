31f741d3dc004dba699d3a20175dfac499f5f851
Ryan Mesa <ryan.mesa@gmail.com>
2024-04-12 04:30:30 -0400

    Update send_email_avatar.php

--- send_email_avatar.php
+++ send_email_avatar.php
@@ -76,7 +76,7 @@ if (!empty($_POST)) {
             // Send the message
             $mail->send () ;
             $successMessage = "<p style='color: green; '>Thank you for contacting us :)</p>";
-            header("Location: http://www.digitaltalentsherpas99.com", true, 301);
+            header("Location: index.html");
             exit();
         } catch (Exception $e) {
             $errorMessage = "<p style='color: red; '>Something went wrong. Please try again later</p>";
